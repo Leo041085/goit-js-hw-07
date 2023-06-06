938eba673ff880d224d7f11ca51306350a5f6e11
Leo041085 <131529700+Leo041085@users.noreply.github.com>
2023-06-06 22:05:16 +0300

    1 part 7 hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,25 +2,46 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 // console.log(galleryItems);
-const cardList = document.querySelector(".gallery");
+const cardList = document.querySelector('.gallery');
 
 const galleryList = galleryItems
   .map(({ preview, original, description }) => {
-    return `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>`;
-  }).join('');
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+  })
+  .join('');
 
 cardList.insertAdjacentHTML('afterbegin', galleryList);
 cardList.addEventListener('click', handlerCardListClick);
 
 function handlerCardListClick(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
+  if (evt.target === evt.currentTarget) return;
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}" />`,
+    {
+      onClose: () => {
+        document.removeEventListener('keydown', handleEscapeKeyPress);
+      },
+    }
+  );
+
+  const handleEscapeKeyPress = evt => {
+    if (evt.code === 'Escape') {
+      instance.close();
+    }
+  };
+
+  instance.show();
+  document.addEventListener('keydown', handleEscapeKeyPress);
 }
